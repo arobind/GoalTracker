d0e75a0c541a7533492b957714162f07bc74d975
arobind <arobind.m@gmail.com>
2021-05-04 04:09:18 +0530

    Made Changes: - Updated the README - Added AddUser and Card Component

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GoalTracker
+- This project is made as a part of React Course by Maximilian Schwarzmüller: https://www.udemy.com/course/react-the-complete-guide-incl-redux
+
---
+++ src/Components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+
+const AddUser = props =>{
+    const addUserHandler = (e)=>{
+        e.preventDefault();
+
+    }
+    return(
+        <Card className ={classes.input} >
+        <form onSubmit={addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="" id="username"/>
+            <label htmlFor="age">Age (Years)</label>
+            <input type="number" name="" id="age"/>
+            <button type="submit">Add User</button>
+        </form>
+    </Card>
+    )
+};
+
+export default AddUser;
